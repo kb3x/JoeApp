a4c606ea0f658fb44041bfefc9d9317c8faa3b93
johnh151 <huynhj94@students.rowan.edu>
2023-03-06 17:40:22 -0500

    pushing again

--- OurPage/main.py
+++ OurPage/main.py
@@ -1,24 +1,12 @@
 # how to use css in python_ flask
 # flask render_template example
  
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
-from flask_mysqldb import MySQLdb
+from flask import Flask, render_template    #, request
 #missing item----------------------------------------------------------
 
 # Provide template folder name
 # The default folder name should be "templates" else need to mention custom folder name
 app = Flask(__name__)
-
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Skywalker88!'
-app.config['MYSQL_DB'] = 'new_schema'
-
-#db=MySQLdb.connector.connect(host="localhost", user="root", password="Skywalker88!",database="new_schema")
-
-mysql = MySQL(app)
 
 
 # @app.route('/')
@@ -31,12 +19,7 @@ def index():
  
 @app.route('/page2/')
 def page2():
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM liquors")
-    data = cursor.fetchall()
-    print (data)
-    #return render_template('index.html', data=data)
-    return render_template('page2.html', data=data)
+    return render_template('page2.html')
 
 @app.route('/addItem/')
 def addItem():
