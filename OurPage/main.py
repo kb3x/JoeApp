036f2ec0e94261278fd475640da1cf6e8dafb0d5
johnh151 <huynhj94@students.rowan.edu>
2023-03-10 00:46:52 -0500

    added navbar pages, rerouted close button in AddItem

--- OurPage/main.py
+++ OurPage/main.py
@@ -29,5 +29,45 @@ def addItem():
 def EditItem():
     return render_template('EditItem.html')
 
+
+#tabs/pages route below---------------------------
+
+@app.route('/beer/')
+def beer():
+    return render_template('tabs/beer.html')
+
+@app.route('/wine/')
+def wine():
+    return render_template('tabs/wine.html')
+
+@app.route('/vodka/')
+def vodka():
+    return render_template('tabs/vodka.html')
+
+@app.route('/rum/')
+def rum():
+    return render_template('tabs/rum.html')
+
+@app.route('/whiskey/')
+def whiskey():
+    return render_template('tabs/whiskey.html')
+
+@app.route('/tequila/')
+def tequila():
+    return render_template('tabs/tequila.html')
+
+@app.route('/gin/')
+def gin():
+    return render_template('tabs/gin.html')
+
+@app.route('/brandy/')
+def brandy():
+    return render_template('tabs/brandy.html')
+
+@app.route('/mezcal/')
+def mezcal():
+    return render_template('tabs/mezcal.html')
+
+
 if __name__=='__main__':
     app.run(debug = True)
