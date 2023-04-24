2f9208da9f1f5bed1791ddd8e49ff8a8e9593f1e
cwhite4017 <chriswhite4017@gmail.com>
2023-04-24 15:58:36 -0400

    mergec

--- OurPage/main.py
+++ OurPage/main.py
@@ -13,10 +13,10 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'ThaoDuong*6'
+app.config['MYSQL_PASSWORD'] = 'Skywalker88!'
 app.config['MYSQL_DB'] = 'new_schema'
 
-db=mysql.connector.connect(host="localhost", user="root", password="ThaoDuong*6",database="new_schema")
+db=mysql.connector.connect(host="localhost", user="root", password="Skywalker88!",database="new_schema")
 
 mysql = MySQL(app)
 
