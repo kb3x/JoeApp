fd43311a9a946b9acaaf3ca34d7cb7d53afcbf6b
cwhite4017 <chriswhite4017@gmail.com>
2023-04-23 21:24:27 -0400

    Dynamic Dropdown2

--- OurPage/main.py
+++ OurPage/main.py
@@ -113,13 +113,6 @@ def search():
 #def EditItem():
  #   return render_template('EditItem.html')
 
-@app.route('/search/')
-def search():
-   cursor = db.cursor()
-   cursor.execute("SELECT name, information, address, notes FROM contact")
-   data = cursor.fetchall()
-   cursor.close()
-   return render_template('search.html', data=data)
 
 @app.route('/searchResults/')
 def searchResults():
